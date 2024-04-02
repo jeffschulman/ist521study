7cd2f2b670661795e279a5e6b09f7bf77984be42
JeffSchulman <contact@jeffschulman.com>
2024-04-02 04:26:45 -0400

    Add index

---
+++ welcome.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Show the 'Continue' button after 30 seconds
+    setTimeout(() => {
+        document.getElementById('continueButton').style.display = 'block';
+    }, 30000);
+});
